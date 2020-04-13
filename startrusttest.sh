eb76374c4e4b1b7fd2fc9371cb4b77817d9c872c
Ubuntu <ubuntu@ip-172-31-33-126.ap-south-1.compute.internal>
2020-04-13 18:49:09 +0000

    added starttest file

---
+++ startrusttest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+guser=$1
+gport=$2
+rcon=$3
+rpass=$4
+players=$5
+seed=$6
+cd $DIR
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DIR/RustDedicated_Data/Plugins/x86_64
+$DIR/RustDedicated  +server.ip 0.0.0.0 +server.port $gport +server.tickrate 10  +rcon.ip 0.0.0.0 +rcon.port $rcon +rcon.password $rpass +rcon.web 1  +server.maxplayers $players +server.hostname "rustserver.io" +server.identity "my_server_identity" +server.level "Procedural Map" +server.seed $seed +server.worldsize 2000 +server.saveinterval 300 +server.globalchat true +server.description "server hosted on rustserver.io" +server.url "rustserver.io"  
+-logfile "logrust.log"
