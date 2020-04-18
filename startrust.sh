51bb782ca02ef35392d2a008df38c227f7459b26
kundan333 <kundanbora77@gmail.com>
2020-04-18 20:40:09 +0000

    some update on startrust

--- startrust.sh
+++ startrust.sh
@@ -8,4 +8,13 @@ players=$6
 seed=$7
 cd $DIR
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DIR/RustDedicated_Data/Plugins/x86_64
+##remove killed
+rm ~/gamefiles/.killed
+##if RustDedicated already running kill it
+##if pgrep Rust
+
 $DIR/RustDedicated  +server.ip 0.0.0.0 +server.port $gport +server.tickrate 10  +rcon.ip 0.0.0.0 +rcon.port $rcon +rcon.password $rpass +rcon.web 1  +server.maxplayers $players +server.hostname "rustserver.io" +server.identity "my_server_identity" +server.level "Procedural Map" +server.seed $seed +server.worldsize 2000 +server.saveinterval 300 +server.globalchat true +server.description "server hosted on rustserver.io" +server.url "rustserver.io"   > ~/gamefiles/optest 2>&1
+
+##if killed
+touch ~/gamefiles/.killed
+
