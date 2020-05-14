1f46fdb7f45bcfbddeaf8385265cea2848ea0969
kundan333 <kundanbora77@gmail.com>
2020-05-15 00:15:22 +0530

    added quote in start rust

--- startrust.sh
+++ startrust.sh
@@ -18,7 +18,7 @@ fi
 ##if RustDedicated already running kill it
 ##if pgrep Rust
 
-$DIR/RustDedicated  +server.ip 0.0.0.0 +server.port $gport +server.tickrate 10  +rcon.ip 0.0.0.0 +rcon.port $rcon +rcon.password $rpass +rcon.web 1  +server.maxplayers $players +server.hostname $hostname +server.identity "my_server_identity" +server.level $map_name +server.seed $seed +server.worldsize 2000 +server.saveinterval 300 +server.globalchat true +server.description "server hosted on rustserver.io" +server.url "rustserver.io"   > ~/gamefiles/.gopt 2>&1
+$DIR/RustDedicated  +server.ip 0.0.0.0 +server.port $gport +server.tickrate 10  +rcon.ip 0.0.0.0 +rcon.port $rcon +rcon.password $rpass +rcon.web 1  +server.maxplayers $players +server.hostname "$hostname" +server.identity "my_server_identity" +server.level "$map_name" +server.seed $seed +server.worldsize 2000 +server.saveinterval 300 +server.globalchat true +server.description "server hosted on rustserver.io" +server.url "rustserver.io"   > ~/gamefiles/.gopt 2>&1
 
 ##if killed
 touch ~/gamefiles/.killed
