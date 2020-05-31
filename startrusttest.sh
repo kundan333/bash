038f6e4ac3fb4150c3502a99ea9aa14c9d2828a4
kundan333 <kundanbora77@gmail.com>
2020-05-31 22:20:39 +0000

    updated

--- startrusttest.sh
+++ startrusttest.sh
@@ -8,5 +8,5 @@ players=$5
 seed=$6
 cd $DIR
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DIR/RustDedicated_Data/Plugins/x86_64
-$DIR/RustDedicated  +server.ip 0.0.0.0 +server.port $gport +server.tickrate 10  +rcon.ip 0.0.0.0 +rcon.port $rcon +rcon.password $rpass +rcon.web 1  +server.maxplayers $players +server.hostname "rustserver.io" +server.identity "my_server_identity" +server.level "Procedural Map" +server.seed $seed +server.worldsize 2000 +server.saveinterval 300 +server.globalchat true +server.description "server hosted on rustserver.io" +server.url "rustserver.io"  
+$DIR/RustDedicated  +server.ip 0.0.0.0 +server.port $gport +server.tickrate 10  +rcon.ip 0.0.0.0 +rcon.port $rcon +rcon.password $rpass +rcon.web 1  +server.maxplayers $players +server.hostname "rustserver.io" +server.identity "my_server_identity" +server.level "Procedural Map" +server.seed $seed  +server.saveinterval 300 +server.globalchat true +server.description "server hosted on rustserver.io" +server.url "rustserver.io"  
 -logfile "logrust.log"
